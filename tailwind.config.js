09211d148d9402acbb371f8dba39d932930090e7
arizig <arizig13@gmail.com>
2025-05-01 17:12:37 +0200

    تعديل القائمة الرئيسية للمشروبات

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-module.exports = {
-  content: ["./*.html"],
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
